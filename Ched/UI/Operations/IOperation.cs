52225e2b2fff8b4f81a8b75ed43dfba94f0f05df
Paralleltree <paralleltree@outlook.com>
2017-12-03 17:26:06 +0900

    OperationManagerクラスを追加

---
+++ Ched/UI/Operations/IOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ched.UI.Operations
+{
+    /// <summary>
+    /// ユーザーの操作を表すインタフェースです。
+    /// </summary>
+    internal interface IOperation
+    {
+        /// <summary>
+        /// この操作の説明を取得します。
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// 操作を元に戻します。
+        /// </summary>
+        void Undo();
+
+        /// <summary>
+        /// 操作をやり直します。
+        /// </summary>
+        void Redo();
+    }
+}
