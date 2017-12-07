f43d6d7039f9b20eb787f2b199127e5d77e198c0
Paralleltree <paralleltree@outlook.com>
2017-12-07 19:20:08 +0900

    ControlExtensionsクラスを追加

---
+++ Ched/UI/ControlExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ched.UI
+{
+    internal static class ControlExtensions
+    {
+        public static LayoutManager WorkWithLayout(this Control control)
+        {
+            return new LayoutManager(control);
+        }
+    }
+
+    internal class LayoutManager : IDisposable
+    {
+        protected Control _control;
+
+        public LayoutManager(Control control)
+        {
+            control.SuspendLayout();
+            _control = control;
+        }
+
+        public void Dispose()
+        {
+            _control.ResumeLayout(false);
+            _control.PerformLayout();
+        }
+    }
+}
