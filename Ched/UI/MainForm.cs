b4506ed4a53bff87903372cfc8af5bdbb66f18d4
Paralleltree <paralleltree@outlook.com>
2017-12-10 18:37:34 +0900

    MainFormに新規ノーツ選択ツールバーを追加

--- Ched/UI/MainForm.cs
+++ Ched/UI/MainForm.cs
@@ -8,15 +8,111 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Ched.Components;
+using Ched.Properties;
+
 namespace Ched.UI
 {
     public partial class MainForm : Form
     {
+        private NoteView NoteView { get; }
+
         public MainForm()
         {
             InitializeComponent();
 
-            this.Controls.Add(new NoteView() { Dock = DockStyle.Fill });
+            ToolStripManager.RenderMode = ToolStripManagerRenderMode.System;
+
+            NoteView = new NoteView() { Dock = DockStyle.Fill };
+
+            using (var manager = this.WorkWithLayout())
+            {
+                this.Controls.Add(NoteView);
+                this.Controls.Add(CreateNewNoteTypeToolStrip(NoteView));
+            }
+
+            NoteView.NewNoteType = NoteType.Tap;
+        }
+
+        private ToolStrip CreateNewNoteTypeToolStrip(NoteView noteView)
+        {
+            var tapButton = new ToolStripButton("TAP", Resources.TapIcon, (s, e) => noteView.NewNoteType = NoteType.Tap)
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+            var exTapButton = new ToolStripButton("ExTAP", Resources.ExTapIcon, (s, e) => noteView.NewNoteType = NoteType.ExTap)
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+            var holdButton = new ToolStripButton("HOLD", Resources.HoldIcon, (s, e) => noteView.NewNoteType = NoteType.Hold)
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+            var slideButton = new ToolStripButton("SLIDE", Resources.SlideIcon, (s, e) => noteView.NewNoteType = NoteType.Slide)
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+            var airActionButton = new ToolStripButton("AIR-ACTION", Resources.AirActionIcon, (s, e) => noteView.NewNoteType = NoteType.AirAction)
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+            var flickButton = new ToolStripButton("FLICK", Resources.FlickIcon, (s, e) => noteView.NewNoteType = NoteType.Flick)
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+            var damageButton = new ToolStripButton("DAMAGE", Resources.DamgeIcon, (s, e) => noteView.NewNoteType = NoteType.Damage)
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+
+            var airKind = new CheckableToolStripSplitButton()
+            {
+                DisplayStyle = ToolStripItemDisplayStyle.Image
+            };
+            airKind.Text = "AIR";
+            airKind.Click += (s, e) => noteView.NewNoteType = NoteType.Air;
+            airKind.DropDown.Items.AddRange(new ToolStripItem[]
+            {
+                new ToolStripMenuItem("振り上げAIR", Resources.AirUpIcon, (s, e) => noteView.AirDirection = new AirDirection(VerticalAirDirection.Up, HorizontalAirDirection.Center)),
+                new ToolStripMenuItem("振り上げ左AIR", Resources.AirLeftUpIcon, (s, e) => noteView.AirDirection = new AirDirection(VerticalAirDirection.Up, HorizontalAirDirection.Left)),
+                new ToolStripMenuItem("振り上げ右AIR", Resources.AirRightUpIcon, (s, e) => noteView.AirDirection = new AirDirection(VerticalAirDirection.Up, HorizontalAirDirection.Right)),
+                new ToolStripMenuItem("振り下げAIR", Resources.AirDownIcon, (s, e) => noteView.AirDirection = new AirDirection(VerticalAirDirection.Down, HorizontalAirDirection.Center)),
+                new ToolStripMenuItem("振り下げ左AIR", Resources.AirLeftDownIcon, (s, e) => noteView.AirDirection = new AirDirection(VerticalAirDirection.Down, HorizontalAirDirection.Left)),
+                new ToolStripMenuItem("振り下げ右AIR", Resources.AirRightDownIcon, (s, e) => noteView.AirDirection = new AirDirection(VerticalAirDirection.Down, HorizontalAirDirection.Right))
+            });
+            airKind.Image = Resources.AirUpIcon;
+
+            noteView.NewNoteTypeChanged += (s, e) =>
+            {
+                tapButton.Checked = noteView.NewNoteType.HasFlag(NoteType.Tap);
+                exTapButton.Checked = noteView.NewNoteType.HasFlag(NoteType.ExTap);
+                holdButton.Checked = noteView.NewNoteType.HasFlag(NoteType.Hold);
+                slideButton.Checked = noteView.NewNoteType.HasFlag(NoteType.Slide);
+                airKind.Checked = noteView.NewNoteType.HasFlag(NoteType.Air);
+                airActionButton.Checked = noteView.NewNoteType.HasFlag(NoteType.AirAction);
+                flickButton.Checked = noteView.NewNoteType.HasFlag(NoteType.Flick);
+                damageButton.Checked = noteView.NewNoteType.HasFlag(NoteType.Damage);
+            };
+
+            noteView.AirDirectionChanged += (s, e) =>
+            {
+                switch (noteView.AirDirection.HorizontalDirection)
+                {
+                    case HorizontalAirDirection.Center:
+                        airKind.Image = noteView.AirDirection.VerticalDirection == VerticalAirDirection.Up ? Resources.AirUpIcon : Resources.AirDownIcon;
+                        break;
+
+                    case HorizontalAirDirection.Left:
+                        airKind.Image = noteView.AirDirection.VerticalDirection == VerticalAirDirection.Up ? Resources.AirLeftUpIcon : Resources.AirLeftDownIcon;
+                        break;
+
+                    case HorizontalAirDirection.Right:
+                        airKind.Image = noteView.AirDirection.VerticalDirection == VerticalAirDirection.Up ? Resources.AirRightUpIcon : Resources.AirRightDownIcon;
+                        break;
+                }
+            };
+
+            return new ToolStrip(new ToolStripItem[] { tapButton, exTapButton, holdButton, slideButton, airKind, airActionButton, flickButton, damageButton });
         }
     }
 }
