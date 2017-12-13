bf943a7fd2347029c337b969dbd20ba6983d6c68
Paralleltree <paralleltree@outlook.com>
2017-12-13 21:32:22 +0900

    BPMChangeEventクラスを追加

---
+++ Ched/Components/Events/BPMChangeEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ched.Components.Events
+{
+    /// <summary>
+    /// BPMの変更イベントを表すクラスです。
+    /// </summary>
+    public class BPMChangeEvent : EventBase
+    {
+        public int BPM { get; set; }
+    }
+}
