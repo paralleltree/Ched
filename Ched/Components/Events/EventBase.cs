8709c70e4fbff182c37033e008f43eacf39a2a24
Paralleltree <paralleltree@outlook.com>
2017-12-13 21:31:40 +0900

    EventBaseクラスを追加

---
+++ Ched/Components/Events/EventBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ched.Components
+{
+    public abstract class EventBase
+    {
+        public int Tick { get; set; }
+    }
+}
