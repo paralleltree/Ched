091d4ae6d70a226bb1269280256ee00e3c9fe7ec
Paralleltree <paralleltree@outlook.com>
2018-03-09 16:50:45 +0900

    BookPropertiesForm追加

---
+++ Ched/UI/BookPropertiesForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Ched.UI
+{
+    partial class BookPropertiesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.artistBox = new System.Windows.Forms.TextBox();
+            this.notesDesignerBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.musicSourceSelector = new Ched.UI.SoundSourceSelector();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(97, 18);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(235, 19);
+            this.titleBox.TabIndex = 0;
+            // 
+            // artistBox
+            // 
+            this.artistBox.Location = new System.Drawing.Point(97, 43);
+            this.artistBox.Name = "artistBox";
+            this.artistBox.Size = new System.Drawing.Size(235, 19);
+            this.artistBox.TabIndex = 1;
+            // 
+            // notesDesignerBox
+            // 
+            this.notesDesignerBox.Location = new System.Drawing.Point(97, 68);
+            this.notesDesignerBox.Name = "notesDesignerBox";
+            this.notesDesignerBox.Size = new System.Drawing.Size(235, 19);
+            this.notesDesignerBox.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.titleBox);
+            this.groupBox1.Controls.Add(this.notesDesignerBox);
+            this.groupBox1.Controls.Add(this.artistBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "基本情報";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "タイトル";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "アーティスト";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ノーツデザイナー";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.musicSourceSelector);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(349, 115);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "音源情報";
+            // 
+            // musicSourceSelector
+            // 
+            this.musicSourceSelector.Location = new System.Drawing.Point(12, 16);
+            this.musicSourceSelector.Name = "musicSourceSelector";
+            this.musicSourceSelector.Size = new System.Drawing.Size(331, 91);
+            this.musicSourceSelector.TabIndex = 0;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(205, 239);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 5;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(286, 239);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 6;
+            this.buttonCancel.Text = "キャンセル";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // BookPropertyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 274);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BookPropertyForm";
+            this.Text = "BookPropertyForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.TextBox artistBox;
+        private System.Windows.Forms.TextBox notesDesignerBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private SoundSourceSelector musicSourceSelector;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ Ched/UI/BookPropertiesForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Ched.Components;
+
+namespace Ched.UI
+{
+    public partial class BookPropertiesForm : Form
+    {
+        public SoundSource MusicSource { get { return musicSourceSelector.Value; } }
+
+        public BookPropertiesForm(ScoreBook book, SoundSource musicSource)
+        {
+            InitializeComponent();
+            Text = "譜面プロパティ";
+            AcceptButton = buttonOK;
+            CancelButton = buttonCancel;
+            buttonOK.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            titleBox.Text = book.Title;
+            artistBox.Text = book.ArtistName;
+            notesDesignerBox.Text = book.NotesDesignerName;
+            if (musicSource != null) musicSourceSelector.Value = musicSource;
+
+            buttonOK.Click += (s, e) =>
+            {
+                book.Title = titleBox.Text;
+                book.ArtistName = artistBox.Text;
+                book.NotesDesignerName = notesDesignerBox.Text;
+                Close();
+            };
+        }
+    }
+}
