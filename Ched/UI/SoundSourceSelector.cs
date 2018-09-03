aae7d5598cb4d20a313b8d6f03e493714385bfe2
Paralleltree <paralleltree@outlook.com>
2018-09-03 12:13:32 +0900

    SoundSourceSelectorに音源ファイルのドラッグドロップを実装

--- Ched/UI/SoundSourceSelector.cs
+++ Ched/UI/SoundSourceSelector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace Ched.UI
 {
     public partial class SoundSourceSelector : UserControl
     {
+        public IEnumerable<string> SupportedExtensions => new string[] { ".wav", ".mp3" };
+
         public SoundSource Value
         {
             get
@@ -26,16 +29,36 @@ namespace Ched.UI
                 latencyBox.Value = (decimal)(value?.Latency ?? 0);
             }
         }
+
         public SoundSourceSelector()
         {
             InitializeComponent();
+
+            AllowDrop = true;
+            DragEnter += (s, e) =>
+            {
+                e.Effect = DragDropEffects.None;
+                if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+
+                var items = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (items.Length != 1) return;
+
+                string path = items.Single();
+                if (SupportedExtensions.Any(p => Path.GetExtension(path) == p) && File.Exists(path))
+                    e.Effect = DragDropEffects.Copy;
+            };
+            DragDrop += (s, e) =>
+            {
+                filePathBox.Text = ((string[])e.Data.GetData(DataFormats.FileDrop)).Single();
+            };
         }
 
         private void buttonBrowse_Click(object sender, EventArgs e)
         {
+            var wildcards = SupportedExtensions.Select(p => "*" + p);
             var dialog = new OpenFileDialog()
             {
-                Filter = "音声ファイル(*.wav, *.mp3)|*.wav;*.mp3"
+                Filter = string.Format("音声ファイル({0})|{1}", string.Join(", ", wildcards), string.Join(";", wildcards))
             };
             if (dialog.ShowDialog(this) == DialogResult.OK) filePathBox.Text = dialog.FileName;
         }
