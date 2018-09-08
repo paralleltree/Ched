ee17262e2ba6eb008ae44f84489a2ba7376ea255
Paralleltree <paralleltree@outlook.com>
2018-09-08 17:39:07 +0900

    PluginManager追加

---
+++ Ched/Plugins/PluginManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Registration;
+
+namespace Ched.Plugins
+{
+    public class PluginManager
+    {
+        private PluginManager()
+        {
+        }
+
+        public static PluginManager GetInstance()
+        {
+            var builder = new RegistrationBuilder();
+            builder.ForTypesDerivedFrom<IPlugin>().ExportInterfaces();
+            builder.ForType<PluginManager>().Export<PluginManager>();
+
+            var self = new AssemblyCatalog(typeof(PluginManager).Assembly, builder);
+            var catalog = new AggregateCatalog(self);
+            // import dlls
+
+            var container = new CompositionContainer(catalog);
+            return container.GetExportedValue<PluginManager>();
+        }
+    }
+}
