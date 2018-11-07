3d6a6647576a14293496232d60d0e2b607588de6
Paralleltree <paralleltree@outlook.com>
2018-11-07 18:03:51 +0900

    Add Air direction enum in Ched.Core

---
+++ Ched.Core/Notes/Air.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ched.Core.Notes
+{
+    public enum VerticalAirDirection
+    {
+        Up,
+        Down
+    }
+
+    public enum HorizontalAirDirection
+    {
+        Center,
+        Left,
+        Right
+    }
+}
