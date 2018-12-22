dff8bafd776d5a879277933d3adca32755eb7336
Paralleltree <paralleltree@outlook.com>
2018-12-22 12:10:19 +0900

    Add SettingsBase class

---
+++ Ched/Configuration/SettingsBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Ched.Configuration
+{
+    internal interface IUpgradable
+    {
+        bool HasUpgraded { get; }
+    }
+
+    internal abstract class SettingsBase : ApplicationSettingsBase, IUpgradable
+    {
+        [UserScopedSetting]
+        [DefaultSettingValue("False")]
+        public bool HasUpgraded
+        {
+            get { return (bool)this["HasUpgraded"]; }
+            private set { this["HasUpgraded"] = value; }
+        }
+
+        public override void Upgrade()
+        {
+            base.Upgrade();
+            HasUpgraded = true;
+            Save();
+        }
+    }
+}
