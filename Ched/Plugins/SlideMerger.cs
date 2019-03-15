adfa8ab7d20cb6a9234b9c8b109e217612433a1c
Paralleltree <paralleltree@outlook.com>
2019-03-16 00:00:14 +0900

    スライド分割/結合プラグイン実装

---
+++ Ched/Plugins/SlideMerger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ched.Core.Notes;
+
+namespace Ched.Plugins
+{
+    public class SlideMerger : IScorePlugin
+    {
+        public string DisplayName => "スライド結合";
+
+        public void Run(IScorePluginArgs args)
+        {
+            var score = args.GetCurrentScore();
+            var range = args.GetSelectedRange();
+            bool modified = false;
+            int startTick = range.Duration < 0 ? range.StartTick + range.Duration : range.StartTick;
+            int endTick = range.Duration < 0 ? range.StartTick : range.StartTick + range.Duration;
+            var endStepDic = score.Notes.Slides.ToDictionary(p => p, p => p.StepNotes.OrderByDescending(q => q.TickOffset).First());
+            var startDic = score.Notes.Slides.Where(p => p.StartTick >= startTick && p.StartTick <= endTick)
+                .Select(p => new { Position = Tuple.Create(p.StartTick, p.StartLaneIndex, p.StartWidth), Note = p })
+                .GroupBy(p => p.Position)
+                .ToDictionary(p => p.Key, p => p.Select(q => q.Note).ToList());
+            var endDic = score.Notes.Slides.Where(p => endStepDic[p].Tick >= startTick && endStepDic[p].Tick <= endTick)
+                .Select(p => new { Position = Tuple.Create(endStepDic[p].Tick, endStepDic[p].LaneIndex, endStepDic[p].Width), Note = p })
+                .GroupBy(p => p.Position)
+                .ToDictionary(p => p.Key, p => p.Select(q => q.Note).ToList());
+
+            while (endDic.Count > 0)
+            {
+                var pos = endDic.First().Key;
+                if (endDic[pos].Count == 0 || !startDic.ContainsKey(pos) || startDic[pos].Count == 0)
+                {
+                    endDic.Remove(pos);
+                    continue;
+                }
+
+                if (startDic[pos].Count > 0)
+                {
+                    var heading = endDic[pos][0];
+                    var trailing = startDic[pos][0];
+                    var trailingOldSteps = trailing.StepNotes.OrderBy(p => p.TickOffset).ToList();
+                    heading.StepNotes.AddRange(trailingOldSteps.Select(p =>
+                    {
+                        var step = new Slide.StepTap(heading) { TickOffset = p.Tick - heading.StartTick, IsVisible = p.IsVisible };
+                        step.SetPosition(p.LaneIndex - heading.StartLaneIndex, p.Width - heading.StartWidth);
+                        return step;
+                    }));
+                    score.Notes.Slides.Remove(trailing);
+                    startDic[pos].Remove(trailing);
+                    var trailingEndPos = Tuple.Create(endStepDic[trailing].Tick, endStepDic[trailing].LaneIndex, endStepDic[trailing].Width);
+                    if (endDic.ContainsKey(trailingEndPos)) endDic[trailingEndPos].Remove(trailing);
+                    endDic[pos].Remove(heading);
+                    if (!endDic.ContainsKey(trailingEndPos)) endDic.Add(trailingEndPos, new[] { heading }.ToList());
+                    else endDic[trailingEndPos].Add(heading);
+                    modified = true;
+                }
+            }
+            if (modified) args.UpdateScore(score);
+        }
+    }
+}
---
+++ Ched/Plugins/SlideSplitter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ched.Core.Notes;
+
+namespace Ched.Plugins
+{
+    public class SlideSplitter : IScorePlugin
+    {
+        public string DisplayName => "スライド分割";
+
+        public void Run(IScorePluginArgs args)
+        {
+            var score = args.GetCurrentScore();
+            var range = args.GetSelectedRange();
+            bool modified = false;
+            var targets = score.Notes.Slides.Where(p => p.StartTick < range.StartTick && p.StepNotes.OrderByDescending(q => q.TickOffset).First().Tick > range.StartTick);
+
+            foreach (var slide in targets.ToList())
+            {
+                // カーソル位置に中継点が存在しなければ処理しない
+                int offset = range.StartTick - slide.StartTick;
+                if (slide.StepNotes.All(p => p.TickOffset != offset)) continue;
+
+                var first = new Slide() { StartTick = slide.StartTick };
+                first.SetPosition(slide.StartLaneIndex, slide.StartWidth);
+                first.StepNotes.AddRange(slide.StepNotes.OrderBy(p => p.TickOffset).TakeWhile(p => p.TickOffset <= offset).Select(p =>
+                {
+                    var step = new Slide.StepTap(first) { TickOffset = p.TickOffset, IsVisible = p.IsVisible };
+                    step.SetPosition(p.LaneIndexOffset, p.WidthChange);
+                    return step;
+                }));
+                first.StepNotes[first.StepNotes.Count - 1].IsVisible = true;
+
+                var second = new Slide() { StartTick = range.StartTick };
+                var trailing = slide.StepNotes.OrderBy(p => p.TickOffset).SkipWhile(p => p.TickOffset < offset).ToList();
+                second.SetPosition(trailing[0].LaneIndex, trailing[0].Width);
+                second.StepNotes.AddRange(trailing.Skip(1).Select(p =>
+                {
+                    var step = new Slide.StepTap(second) { TickOffset = p.TickOffset - offset, IsVisible = p.IsVisible };
+                    step.SetPosition(p.LaneIndex - second.StartLaneIndex, p.Width - second.StartWidth);
+                    return step;
+                }));
+
+                score.Notes.Slides.Add(first);
+                score.Notes.Slides.Add(second);
+                score.Notes.Slides.Remove(slide);
+                modified = true;
+            }
+            if (modified) args.UpdateScore(score);
+        }
+    }
+}
