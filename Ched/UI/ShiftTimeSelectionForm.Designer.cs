17da4ba87005e41d86799d2656be3e5ef1ab6b4c
Paralleltree <paralleltree@outlook.com>
2020-10-30 22:55:43 +0900

    Add ShiftTimeSelectionForm

---
+++ Ched/UI/ShiftTimeSelectionForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Ched.UI
+{
+    partial class ShiftTimeSelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.durationTypeBox = new System.Windows.Forms.ComboBox();
+            this.countBox = new System.Windows.Forms.NumericUpDown();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.countBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // durationTypeBox
+            // 
+            this.durationTypeBox.FormattingEnabled = true;
+            this.durationTypeBox.Location = new System.Drawing.Point(172, 16);
+            this.durationTypeBox.Name = "durationTypeBox";
+            this.durationTypeBox.Size = new System.Drawing.Size(69, 20);
+            this.durationTypeBox.TabIndex = 0;
+            // 
+            // countBox
+            // 
+            this.countBox.Location = new System.Drawing.Point(71, 17);
+            this.countBox.Name = "countBox";
+            this.countBox.Size = new System.Drawing.Size(95, 19);
+            this.countBox.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(99, 59);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(181, 59);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "キャンセル";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "挿入時間";
+            // 
+            // ShiftTimeSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 94);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.countBox);
+            this.Controls.Add(this.durationTypeBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ShiftTimeSelectionForm";
+            this.Text = "時間挿入";
+            ((System.ComponentModel.ISupportInitialize)(this.countBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox durationTypeBox;
+        private System.Windows.Forms.NumericUpDown countBox;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label1;
+    }
+}
