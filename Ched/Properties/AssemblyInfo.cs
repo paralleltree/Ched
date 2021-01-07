df7b133a4aaba10158728b6032ffc484a9b54064
Paralleltree <paralleltree@outlook.com>
2021-01-07 15:43:27 +0900

    Add DisableDpiAwareness attribute to Ched

--- Ched/Properties/AssemblyInfo.cs
+++ Ched/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Media;
 
 [assembly: AssemblyTitle("Ched")]
 [assembly: AssemblyDescription("Yet Another Chart Editor")]
@@ -12,3 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("4c0c9f98-6fcf-4d2f-b821-37a66362dc75")]
 
 [assembly: AssemblyVersion("2.8.1.0")]
+
+[assembly: DisableDpiAwareness]
